033041cdd76bc3abe53a2e284099becebb2f8e81
tqx5201 <tqx5201@163.com>
2023-07-05 22:31:21 +0800

    Update settings.py

--- epg/settings.py
+++ epg/settings.py
@@ -72,14 +72,13 @@ WSGI_APPLICATION = 'epg.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
 }
-psql "postgres://default:k8Hr6Uzoaqpd@ep-late-feather-304688.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+#psql "postgres://default:k8Hr6Uzoaqpd@ep-late-feather-304688.us-east-1.postgres.vercel-storage.com:5432/verceldb"
 '''
 DATABASES = {
     'default': {
@@ -93,6 +92,7 @@ DATABASES = {
         'PORT': '5432' # 数据库服务的端口号
     }
 }
+'''
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
